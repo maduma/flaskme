9561c3342fa5244e06d0fc1d73ebd1193d6dcb51
Stéphane Nsakala <stephane.nsakala@luxairgroup.lu>
2019-06-12 15:02:15 +0000

    change to v2

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello my name is Nicolas\n"
+
+@app.route("/health")
+def health_check():
+    return jsonify(status='ok')
